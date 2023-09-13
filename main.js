c28e3925ad482c9607eb841c3d1b3fd9f65aa3dd
Gerard Llorach <gerard.llorach@gmail.com>
2023-09-13 16:56:15 +0200

    vertices manually projected in front of camera

--- main.js
+++ main.js
@@ -45,34 +45,84 @@ let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-// GRID
+// GRID HELPER
 let size = 10;
 let divisions = 10;
 const gridHelper = new THREE.GridHelper( size, divisions );
 scene.add( gridHelper );
 
-// PLANE
+// OCEAN GRID
 size = 5;
 divisions = 10;
-geometry = new THREE.PlaneGeometry( size, size, divisions, divisions );
-let geomWireframe = new THREE.WireframeGeometry( geometry);
-let line = new THREE.LineSegments( geomWireframe );
-line.material.depthTest = false;
-line.material.opacity = 1.0;
-line.material.transparent = true;
+let planeGeometry = new THREE.PlaneGeometry( size, size, divisions, divisions );
+let planeGeometryDefault = new THREE.PlaneGeometry( size, size, divisions, divisions );
 
-scene.add( line );
+let oceanGrid = new THREE.LineSegments( new THREE.WireframeGeometry( planeGeometry));
+oceanGrid.material.depthTest = false;
+oceanGrid.material.opacity = 1.0;
+oceanGrid.material.transparent = true;
+oceanGrid.frustrumculled = false;
+
+scene.add( oceanGrid );
 
 
 
+let tempVec4 = new THREE.Vector4();
+let tempVec4a = new THREE.Vector4();
+function updatePlane(){
+  // Temporal, moves the vertices in front of camera using THREE operations
+  //updateObjectMatrixAccordingToCamera(oceanGrid);
+  //oceanGrid.translateZ(-10);
+
+  let defaultVertices = planeGeometryDefault.attributes.position.array;
+  let vertices = planeGeometry.attributes.position.array;
+  let numVertices = vertices.length/3;
+  
+
+  for (let i = 0; i < numVertices; i++){
+    // Use temp vector
+    tempVec4.set(defaultVertices[i*3], defaultVertices[i*3 + 1], defaultVertices[i*3 + 2], 1);
+    // Move vertices in front of the camera
+    camera.translateZ(-10);
+    camera.updateMatrix();
+    tempVec4 = tempVec4.applyMatrix4(camera.matrix, tempVec4);
+    camera.translateZ(10);
+    // Homogeneous division if needed
+    if (tempVec4.w != 0)
+      tempVec4.divideScalar(tempVec4.w);
+
+    // Calculate intersection with xz plane and camera ray
+
+    // Reassign position to vertex
+    if (isNaN(tempVec4.x) || isNaN(tempVec4.y) || isNaN(tempVec4.z) || isNaN(tempVec4.w)){
+      debugger;
+    }
+    vertices[i*3] = tempVec4.x;
+    vertices[i*3 + 1] = tempVec4.y; 
+    vertices[i*3 + 2] = tempVec4.z;
+  }
+  //console.log(camera.matrix.elements[0]);
+  //console.log(tempVec4.z);
+  //console.log(defaultVertices[0])
+  // Reset matrx
+
+  // Update geometry
+  let geom = oceanGrid.geometry;
+  oceanGrid.geometry.dispose();
+  oceanGrid.geometry = new THREE.WireframeGeometry(geom.parameters.geometry);
+  
+}
 
 
+
+// Make as if it was a child of camera
 function updateObjectMatrixAccordingToCamera(node){
   node.position.copy( camera.position );
   node.rotation.copy( camera.rotation );
   node.updateMatrix();
-  node.translateZ( - 10 );
 }
+
+
 
 
 
@@ -114,7 +164,7 @@ function animate() {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
-  updateObjectMatrixAccordingToCamera(line);
+  updatePlane();
 
 	renderer.render( scene, camera );
 
